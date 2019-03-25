521b3fea5dabdca921d314c0e15f19a5a84aa6e0
dejankovac11 <kovac.dejan.montenegro@gmail.com>
2019-03-25 10:18:53 +0100

    Modifications to Main class, cleaned up

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,51 +4,68 @@ import com.google.api.services.sheets.v4.model.ValueRange;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Vector;
 
 //svaki katalog je jedan valueRange koji se sastoji iz imena (koji treba biti header
 //i liste knjiga
 //listu knjiga treba otpakovati tako da pokazuje svoje vrednosti
 
+
 public class Main
 {
-    //popraviti tako sto treba probati preko kataloga
+    static List<List<Object> > createBodyValues(Catalog catalog)
+    {
+        List<List<Object> >bodyValues=new ArrayList<List<Object>>();
+        List<Book> books=catalog.getBookList();
+        for(Book book:books)
+        {
+            List<String> bodyValuesArgs=new ArrayList<String>(); //contents of a single book
+            bodyValuesArgs.add(book.getTitle());
+            bodyValuesArgs.add(book.getId());
+            bodyValuesArgs.add(book.getAuthor());
+            bodyValuesArgs.add(book.getPublishDate().toString());
+            bodyValuesArgs.add(book.getDescription());
+            bodyValuesArgs.add(((Double) book.getPrice()).toString());
+            List<Object>bodyValuesObjectArgs=new ArrayList<Object>(bodyValuesArgs);
+            bodyValues.add(bodyValuesObjectArgs);
+        }
+        return bodyValues;
+    }
+
+
+    static String calculateStartingIndex(int startingCatalogIndex)
+    {
+        return "A"+((Integer)startingCatalogIndex).toString();
+    }
+
+    static int updateStartingIndex(int startingCatalogIndex,int valueRangeLength)
+    {
+        return startingCatalogIndex+valueRangeLength+2;
+    }
+
+
     public static void main(String[] args) throws IOException, GeneralSecurityException
     {
         SpreadSheetWriter.setup(); //iza haube zapocinje konekciju i autorizaciju
         SpreadSheetWriter.createSpreedsheet(); //kreira novi prazni spreadsheet
         BookUnmarshaller bookUnmarshaller=new BookUnmarshaller();
-        List<Catalog> catalogs=bookUnmarshaller.readXML(); //kreiranje i ucitavanje XML koda u Catalog objekte
+        List<Catalog> catalogs=bookUnmarshaller.readXML(); //kreiranje i ucitavanje XML koda u listu Catalog objekata
         List<ValueRange> body=new ArrayList<ValueRange>(); //Content of the new spreadsheet
-        int startingIndex=1; //where to begin writing new ValueRange object
-        for (Catalog catalog:catalogs) //sve ovo bi mogla da bude static helper funkcija?
+        int startingCatalogIndex=1; //where to begin writing new ValueRange object
+        int valueRangeLength;
+        for (Catalog catalog:catalogs)
         {
+            valueRangeLength=catalog.getBookList().size();
             ValueRange valueRange=new ValueRange(); //valueRange for the current catalog
-            String startingIndexString="A"+((Integer)startingIndex).toString(); //each new catalog begins at A1,A3,A5...
-            valueRange.setRange(startingIndexString); //Initialize starting field for writing the contents of current catalogue
+            String startingCatalogIndexString=calculateStartingIndex(startingCatalogIndex);//each new catalog begins at A1,A3,A5...
+            valueRange.setRange(startingCatalogIndexString); //Initialize starting field for writing the contents of current catalogue
 
             //construct the argument of the setValues method called on current valueRange object
             //bodyValues represents the content of a single catalog
-            List<List<Object> >bodyValues=new ArrayList<List<Object>>();
-            List<Book> books=catalog.getBookList();
-             //arguments for bodyValues
-            for(Book book:books) //for each book
-            {
-                List<String> bodyValuesArgs=new ArrayList<String>();
-                bodyValuesArgs.add(book.getTitle());
-                bodyValuesArgs.add(book.getId());
-                bodyValuesArgs.add(book.getAuthor());
-                bodyValuesArgs.add(book.getPublishDate().toString());
-                bodyValuesArgs.add(book.getDescription());
-                bodyValuesArgs.add(((Double) book.getPrice()).toString());
-                List<Object>bodyValuesObjectArgs=new ArrayList<Object>(bodyValuesArgs);
-                bodyValues.add(bodyValuesObjectArgs);
-            }
+            List<List<Object> >bodyValues=createBodyValues(catalog);
             valueRange.setValues(bodyValues);
             body.add(valueRange);
-            startingIndex+=2;
+            startingCatalogIndex=updateStartingIndex(startingCatalogIndex,valueRangeLength);
         }
         BatchUpdateValuesRequest batchBody=new BatchUpdateValuesRequest().setValueInputOption("RAW").setData(body);
         BatchUpdateValuesResponse batchResult = SpreadSheetWriter.sheetsService.spreadsheets().values().batchUpdate(SpreadSheetWriter.SPREADSHEET_ID,batchBody).execute();
